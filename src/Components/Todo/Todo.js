e01fdd55189a9b9c867f81e7e8332c974abff057
Vladimir RM <shvdrummer@gmail.com>
2022-10-28 20:41:39 +0300

    add  const handleOpenDialog

--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -4,20 +4,24 @@ import TodoAction from "../Todo-actions/TodoAction";
 import TodoRender from "../TodoRender/TodoRender";
 import "./Todo.css";
 
-const initialFormData = 
+const initialFormData = {
+  isEdit: false,
+  todoName: "",
+  todoNote: "",
+  isFinished: false,
+  id: "",
+  index: null,
+};
 
 const Todo = () => {
   const [tab, setTab] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
   const [isOpenDisplayTodo, setIsOpenDisplayTodo] = useState(false);
   const [todo, setTodo] = useState([]);
-  const [formData, setFormData] = useState({
-    todoName: "",
-    todoNote: "",
-    isFinished: false,
-    id: '',
-    index:null
-  });
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleOpenDialog = setIsOpen((prevState)=>(!prevState))
+
   return (
     <div className="todo-wrapper">
       <TodoHeader />
