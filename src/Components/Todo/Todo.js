d23f5a76012b3dd4bf4d68de250184c2a7f081c2
Vladimir RM <shvdrummer@gmail.com>
2022-10-28 20:25:09 +0300

    add const initialFormData

--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -4,12 +4,20 @@ import TodoAction from "../Todo-actions/TodoAction";
 import TodoRender from "../TodoRender/TodoRender";
 import "./Todo.css";
 
+const initialFormData = 
+
 const Todo = () => {
   const [tab, setTab] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
   const [isOpenDisplayTodo, setIsOpenDisplayTodo] = useState(false);
   const [todo, setTodo] = useState([]);
-  const [formData, setFormData] = useState([]);
+  const [formData, setFormData] = useState({
+    todoName: "",
+    todoNote: "",
+    isFinished: false,
+    id: '',
+    index:null
+  });
   return (
     <div className="todo-wrapper">
       <TodoHeader />
